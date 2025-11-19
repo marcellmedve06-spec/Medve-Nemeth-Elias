9cce1d6b5d98ea1adcb0207d93c3c59797efc556
Krampusz65 <95422670+Krampusz65@users.noreply.github.com>
2025-11-19 11:46:57 +0100

    Add files via upload
    Signed-off-by: Krampusz65 <95422670+Krampusz65@users.noreply.github.com>

---
+++ negyzet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+for i in range(4):
+    robot.straight(500)
+    robot.turn(90)
